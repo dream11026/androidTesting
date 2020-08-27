567ce492b6124430801994f1e222305bb610bf7c
張詠齊 <yungchi@cathaylife.com.tw>
2020-08-27 17:20:30 +0800

    build:增加UI Test的features參數，用於測試限定(crucial、medium、low)的分類功能

--- app/src/androidTest/java/com/yungchi/testing/test/CucumberBDDTestRunner.kt
+++ app/src/androidTest/java/com/yungchi/testing/test/CucumberBDDTestRunner.kt
@@ -7,29 +7,39 @@ import cucumber.api.SnippetType
 import cucumber.api.android.CucumberAndroidJUnitRunner
 
 @CucumberOptions(
-    features = ["features"]
-    , plugin = ["pretty"]
+    //features = ["features"] //change for onCreate
+    plugin = ["pretty"]
     , monochrome = true // console output from Cucumber in a readable format
     , snippets = SnippetType.CAMELCASE // SnippetType.CAMELCASE | SnippetType.UNDERSCORE
 )
 class CucumberBDDTestRunner : CucumberAndroidJUnitRunner() {
     override fun onCreate(bundle: Bundle) {
         val tags = BuildConfig.TEST_TAGS
-        if ("" != tags) {
+        if ("".isNotBlank()) {
             bundle.putString(CUCUMBER_TAGS_KEY, tags)
         }
         var scenario = BuildConfig.TEST_SCENARIO
-        if ("" != scenario) {
+        if (scenario.isNotBlank()) {
             scenario = scenario.replace(" ".toRegex(), "\\\\s")
             bundle.putString(CUCUMBER_SCENARIO_KEY, scenario)
         }
 
+        var features = BuildConfig.TEST_FEATURES
+        if (features.isNotBlank()) {
+            features = "features/${features.toLowerCase()}"
+            bundle.putString(CUCUMBER_FEATURES_KEY, features)
+        } else {
+            features = "features"
+        }
+
+        bundle.putString(CUCUMBER_FEATURES_KEY, features) // "features/"+ main
         super.onCreate(bundle)
     }
 
     companion object {
         private val TAG: String = CucumberBDDTestRunner::class.java.simpleName
         private const val CUCUMBER_TAGS_KEY = "tags"
+        private const val CUCUMBER_FEATURES_KEY = "features"
         private const val CUCUMBER_SCENARIO_KEY = "name"
     }
 }
