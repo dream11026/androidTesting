ed591f22201b1cef5f96ef8aad3ee29f250c91ef
張詠齊 <yungchi@cathaylife.com.tw>
2020-08-27 10:07:47 +0800

    使用spoon結合cucumber套件的報告設定

--- app/src/androidTest/java/com/yungchi/testing/test/CucumberBDDTestRunner.kt
+++ app/src/androidTest/java/com/yungchi/testing/test/CucumberBDDTestRunner.kt
@@ -1,10 +1,12 @@
 package com.yungchi.testing.test
 
 import android.os.Bundle
+import android.util.Log
 import com.yungchi.testing.BuildConfig
 import cucumber.api.CucumberOptions
 import cucumber.api.SnippetType
 import cucumber.api.android.CucumberAndroidJUnitRunner
+import java.io.File
 
 @CucumberOptions(
     features = ["features"]
@@ -24,7 +26,59 @@ class CucumberBDDTestRunner : CucumberAndroidJUnitRunner() {
             bundle.putString(CUCUMBER_SCENARIO_KEY, scenario)
         }
 
+//        grantPermission()
+
+        bundle.putString(
+            "plugin", getPluginConfigurationString()
+        ); // we programmatically create the plugin configuration
         super.onCreate(bundle)
+    }
+
+//    private fun grantPermission() {
+//        val permissions: ArrayList<String> = ArrayList()
+//        permissions.add(Manifest.permission.READ_EXTERNAL_STORAGE)
+//        permissions.add(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+//
+//        val command = java.lang.String.format(
+//            "pm grant %s ",
+//            targetContext.packageName
+//        )
+//        //add here your other permissions
+//        for (i in 0 until permissions.size) {
+//            val commandPermissions = command + permissions[i]
+//            getInstrumentation().uiAutomation.executeShellCommand(commandPermissions)
+//            // wait a bit until the command is finished
+//            SystemClock.sleep(1000)
+//        }
+//    }
+
+    /**
+     * Since we want to checkout the external storage directory programmatically, we create the plugin configuration
+     * here, instead of the [CucumberOptions] annotation.
+     *
+     * @return the plugin string for the configuration, which contains XML, HTML and JSON paths
+     */
+    private fun getPluginConfigurationString(): String? {
+        val cucumber = "cucumber"
+        val separator = "--"
+        var reportDir = getAbsoluteFilesPath()
+        Log.d(TAG, "getAbsoluteFilesPath $reportDir")
+        return "pretty" +
+                separator + "html:" + reportDir+ "/" + cucumber + ".html" +
+                separator + "junit:" + reportDir + "/" + cucumber + ".xml" +
+                separator + "json:" + reportDir + "/" + cucumber + ".json"
+    }
+
+    /**
+     * The path which is used for the report files.
+     *
+     * @return the absolute path for the report files
+     */
+    private fun getAbsoluteFilesPath(): String {
+        ///storage/emulated/0/Android/data/[packageName]/reports
+        val directory: File? = targetContext.getExternalFilesDir(null)
+//        val directory: File? = Environment.getExternalStoragePublicDirectory(null)
+        return File(directory, "reports").getAbsolutePath()
     }
 
     companion object {
