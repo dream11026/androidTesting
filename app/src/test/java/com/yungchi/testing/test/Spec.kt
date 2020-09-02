57e0380d15f934b059bf88cb5c01d3419c5b6d4d
張詠齊 <yungchi@cathaylife.com.tw>
2020-09-02 16:42:56 +0800

    update: unit test add mockito case

--- app/src/test/java/com/yungchi/testing/test/Spec.kt
+++ app/src/test/java/com/yungchi/testing/test/Spec.kt
@@ -1,6 +1,8 @@
 package com.yungchi.testing.test
 
 import junit.framework.TestCase.assertEquals
+import org.mockito.Mockito
+import org.mockito.Mockito.`when`
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
@@ -9,11 +11,23 @@ import org.spekframework.spek2.style.specification.describe
  */
 object Spec : Spek({
     describe("A set") {
+        // mock creation
+        val mockedList: List<String> = Mockito.mock(MutableList::class.java) as MutableList<String>
+        `when`(mockedList[0]).thenReturn("one")
+
         val set by memoized { mutableSetOf<String>() }
 
         context("is empty") {
             beforeEachTest {
                 set.add("item")
+            }
+
+            it("mockedList[0] should have one string") {
+                println("mockedList ${mockedList}")
+                println("mockedList[0] ${mockedList[0]}")
+                println("mockedList[1] ${mockedList[1]}")
+                assertEquals(mockedList[0], "one")
+                assertEquals(mockedList[1], null)
             }
 
             //Error case
