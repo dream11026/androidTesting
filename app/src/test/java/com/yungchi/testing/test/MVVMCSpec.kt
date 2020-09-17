688c42a3346e9f96d156fdacd4da86ba148793d8
張詠齊 <yungchi@cathaylife.com.tw>
2020-09-17 14:21:50 +0800

    update: 1.調整gradle task名稱 2.調整fastlane 3.調整MVVM test case

--- app/src/test/java/com/yungchi/testing/test/MVVMCSpec.kt
+++ app/src/test/java/com/yungchi/testing/test/MVVMCSpec.kt
@@ -1,8 +1,16 @@
 package com.yungchi.testing.test
 
-import androidx.lifecycle.Observer
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.MutableLiveData
+import com.yungchi.testing.model.EventTypeSpinModel
+import com.yungchi.testing.test.MVVMCSpec.viewModel
 import com.yungchi.testing.viewmodel.LoginActivityViewModel
-import junit.framework.TestCase.assertEquals
+import org.junit.Assert.assertEquals
+import org.junit.Rule
+import org.mockito.InjectMocks
+import org.mockito.Mock
+import org.mockito.Mockito.mock
+import org.mockito.MockitoAnnotations
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
 
@@ -12,24 +20,54 @@ import org.spekframework.spek2.style.specification.describe
 object MVVMCSpec : Spek({
     describe("MVVMC sample") {
 //        val context: Context = ApplicationProvider.getApplicationContext()
-//        val loginActivityCoordinator: LoginActivityCoordinator<LoginActivity> =
-//            LoginActivityCoordinator(context, LoginActivity::class)
 
+//        var mEventTypeList = Mockito.spy(MutableLiveData<List<EventTypeSpinModel>>())
+//        var eventTypePosition = Mockito.spy(MutableLiveData<Int>())
+//        val viewModel: LoginActivityViewModel = Mockito.spy(LoginActivityViewModel::class.java)
+
+//        val viewModel by memoized { LoginActivityViewModel() }
 
         context("unit test MVVMC") {
-            val viewModel = LoginActivityViewModel()
             beforeEachTest {
+//                Mockito.`when`(viewModel.mEventTypeList.value).thenReturn(listOf<EventTypeSpinModel>(
+//                    EventTypeSpinModel(
+//                        "11",
+//                        "11",
+//                        "11",
+//                        "11"
+//                    ), EventTypeSpinModel(
+//                        "22",
+//                        "22",
+//                        "22",
+//                        "22"
+//                    )
+//                ))
             }
-
             it("should have a size > 0") {
                 //TODO observeForever有問題
-                viewModel.mEventTypeList.observeForever(Observer { data ->
-                    assertEquals("", data[0])
-                })
+//                viewModel.mEventTypeList.observeForever(Observer { data ->
+//                    assertEquals("11", data[0])
+//                })
 
                 viewModel.initEventTypeList()
+                assertEquals("11", viewModel.mEventTypeList.value?.get(0))
             }
 
         }
     }
-})
+}) {
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
+
+    @Mock
+    lateinit var mEventTypeList: MutableLiveData<List<EventTypeSpinModel>>
+
+    @InjectMocks
+    lateinit var viewModel: LoginActivityViewModel
+
+    init {
+        MockitoAnnotations.initMocks(this)
+    }
+}
+
+inline fun <reified T> lambdaMock(): T = mock(T::class.java)
